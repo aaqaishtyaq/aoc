f90f46f7c7211e348c125b4ff20432c40d1177f5
Aaqa Ishtyaq <aaqaishtyaq@gmail.com>
2022-12-03 17:42:10 +0530

    2022/02: add day02 solution
    Signed-off-by: Aaqa Ishtyaq <aaqaishtyaq@gmail.com>

--- pkg/year2022/root.go
+++ pkg/year2022/root.go
@@ -1,14 +1,16 @@
 package year2022
 
 import (
-	"github.com/aaqaishtyaq/aoc/pkg/year2022/day01"
 	"github.com/aaqaishtyaq/aoc/pkg/registry"
+	"github.com/aaqaishtyaq/aoc/pkg/year2022/day01"
+	"github.com/aaqaishtyaq/aoc/pkg/year2022/day02"
 )
 
 func Register() registry.Registry {
 	registery := registry.NewRegistry()
 
 	registery.Add(1, day01.Solution)
+	registery.Add(2, day02.Solution)
 
 	return registery
 }
